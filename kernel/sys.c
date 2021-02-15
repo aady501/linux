3410bfc5e1879ebe281962426b3be558c85ab69b
aadil <aadil.shaikh1703@gmail.com>
2021-02-15 17:23:14 -0500

    move syscall_encrypt to separate file and update make file

--- kernel/sys.c
+++ kernel/sys.c
@@ -2692,38 +2692,3 @@ COMPAT_SYSCALL_DEFINE1(sysinfo, struct compat_sysinfo __user *, info)
 	return 0;
 }
 #endif /* CONFIG_COMPAT */
-SYSCALL_DEFINE2(s2_encrypt, char *, msg, int, num)
-{
-		  if ((num>=1) && (num<=5)){
-			  char *buf;
-			  long copied;
-			  int i=0;
-			  int len_str = strnlen_user(msg, 32767);
-			  printk("len of string:%d",len_str);
-			  if ( ( buf = (char *)kmalloc(len_str, GFP_KERNEL) ) == NULL){
-				  printk(KERN_ERR "kmalloc failed");
-				  kfree(buf);
-				  return -EINVAL;			  
-			  }
-			  copied = strncpy_from_user(buf, msg, len_str);
-			  if(copied<0){
-				  printk(KERN_ERR "Copy failed using strncpy_from_user\n");
-				  kfree(buf);
-				  return -EINVAL;
-			  }
-			  /* Move pointer to end of string
-			   * -1 is added as strnlen_user returns length with \0*/
-			  buf = buf + (len_str-1); 
-			  for (i=0;i<len_str-1;i++){
-				  buf--;
-				  *(buf) = *(buf) + num;
-			  }
-			  printk("ENCRYPTED STRING: %s\n", buf);
-			  kfree(buf);
-			  return 0;
-		  }
-		  else{
-		  	printk(KERN_ERR "Key out of range, valid range(1-5): %d\n", num);
-		  	return -EINVAL;
-		  }
-}
